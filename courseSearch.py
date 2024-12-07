2a72276d7fbe54812b052bd94925cc20fe37da32
scc2450 <111857396+scc2450@users.noreply.github.com>
2024-12-07 18:21:20 +0800

    Add files via upload

--- courseSearch.py
+++ courseSearch.py
@@ -4,6 +4,7 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 st.set_page_config(page_title='PKU Course Search', layout='wide')
+st.header('White Whale Univ. Course Search')
 st.image('https://dean.pku.edu.cn/service/web/images/banner/banner_net.jpg', use_column_width=True)
 def soup_parser(course_data):
     for course in course_data:
@@ -38,8 +39,8 @@ payload['coursename'] = col1.text_input('课程名称/课程号/关键字')
 payload['teachername'] = col2.text_input('教师名称/关键字')
 # 年份与学期
 year_now = pd.Timestamp.now().year
-year = col1.number_input('年份', min_value=2000, max_value=year_now, value=year_now)
-semester_now = '春季' if pd.Timestamp.now().month < 7 else '秋季' if pd.Timestamp.now().month > 8 else '暑校'
+year = col1.number_input('年份', min_value=2000, max_value=year_now+1, value=year_now)
+semester_now = '春季' if pd.Timestamp.now().month < 7 or year == year_now+1 else '秋季' if pd.Timestamp.now().month > 8 else '暑校'
 semester = col2.selectbox('学期', ['春季', '秋季', '暑校'], index=0 if semester_now == '春季' else 1 if semester_now == '秋季' else 2)
 year = year if semester == '秋季' else year - 1
 payload['yearandseme'] = f'{year-2000}-{year-1999}-{1 if semester == "秋季" else 2 if semester == "春季" else 3}'
@@ -62,41 +63,65 @@ format_coursetype = lambda x: {
 coursetype = st.selectbox('课程类型', coursetype_list)
 payload['coursetype'] = format_coursetype(coursetype)
 # st.write(payload['coursetype'])
-yuanxi_list = ['全部', '数院', '物院', '化院', '生科', '地空', '工院', '信科', '心院', '软微', '新传', '中文', '历史', '考古', '哲学', '国关', '经院', '国发', '光华', '法院', '信管', '社系', '政管', '大英', '外院', '马院', '体教', '艺术', '元培', '城环', '环科', '教务部', '医学部']
+yuanxi_list = [
+    '全部-ALL', '数院-SMS', '物院-PHY', '化院-CHEM', '生科-BIO', '地空-SESS', '工院-COE', '信科-CSEE', 
+    '心院-PSY', '软微_SS', '新传-SJC', '中文-CHN', '历史-HIST', '考古-ARC', '哲学-PHIL', '国关-SIS', 
+    '经院-ECO', '国发-NSD', '光华-GSM', '汇丰-PHBS', '法院-LAW', '信管-IM', '社系-SOCI', '政管-SG', 
+    '大英-ENG', '外院-SFL', '马院-MAR', '体教-PE', '艺术-SOA', '元培-YPC', '城环-UES', '环科-CESE', 
+    '材料-MSE', '叉院-AAIS', '教务部-DEAN', '医学部-BJMU', '武装部-XGB', '教育学院-GSE', '歌剧研究院-OPE', 
+    '现代农学院-SAAS', '人口研究所-IPR', '人工智能研究院-AI', '建筑与景观学院-CALA', '对外汉语教育学院-HANYU', 
+    '产业技术研究院-KJKFB', '中国社科调查中心-ISSS', '创新创业学院-NEW', '共青团北大委员会-YOUTH'
+]
 format_yuanxi = lambda x: {
-    '全部': '0',
-    '数院': '00001',
-    '物院': '00004',
-    '化院': '00010',
-    '生科': '00011',
-    '地空': '00012',
-    '工院': '00086',
-    '信科': '00048',
-    '心院': '00016',
-    '软微': '00017',
-    '新传': '00018',
-    '中文': '00020',
-    '历史': '00021',
-    '考古': '00022',
-    '哲学': '00023',
-    '国关': '00024',
-    '经院': '00025',
-    '国发': '00062',
-    '光华': '00028',
-    '法院': '00029',
-    '信管': '00030',
-    '社系': '00031',
-    '政管': '00032',
-    '大英': '00038',
-    '外院': '00039',
-    '马院': '00040',
-    '体教': '00041',
-    '艺术': '00043',
-    '元培': '00046',
-    '城环': '00126',
-    '环科': '00127',
-    '教务部': '00612',
-    '医学部': '10180',
+    '全部-ALL': '0',
+    '数院-SMS': '00001',
+    '物院-PHY': '00004',
+    '化院-CHEM': '00010',
+    '生科-BIO': '00011',
+    '地空-SESS': '00012',
+    '工院-COE': '00086',
+    '信科-CSEE': '00048',
+    '心院-PSY': '00016',
+    '软微_SS': '00017',
+    '新传-SJC': '00018',
+    '中文-CHN': '00020',
+    '历史-HIST': '00021',
+    '考古-ARC': '00022',
+    '哲学-PHIL': '00023',
+    '国关-SIS': '00024',
+    '经院-ECO': '00025',
+    '国发-NSD': '00062',
+    '光华-GSM': '00028',
+    '汇丰-PHBS': '00201',     
+    '法院-LAW': '00029',
+    '信管-IM': '00030',
+    '社系-SOCI': '00031',
+    '政管-SG': '00032',
+    '大英-ENG': '00038',
+    '外院-SFL': '00039',
+    '马院-MAR': '00040',
+    '体教-PE': '00041',
+    '艺术-SOA': '00043',
+    '元培-YPC': '00046',
+    '城环-UES': '00126',
+    '环科-CESE': '00127',
+    '材料-MSE': '00232',
+    '叉院-AAIS': '00084',
+    '教务部-DEAN': '00612',
+    '医学部-BJMU': '10180',
+    '武装部-XGB': '00607',
+    '教育学院-GSE': '00067',
+    '歌剧研究院-OPE': '00192',
+    '现代农学院-SAAS': '00211',
+    '人口研究所-IPR': '00068',
+    '人工智能研究院-AI': '00225',
+    '建筑与景观学院-CALA': '00195',
+    '对外汉语教育学院-HANYU': '00044',
+    '产业技术研究院-KJKFB': '00199',
+    '中国社科调查中心-ISSS': '00187',
+    '创新创业学院-NEW': '00671',
+    '共青团北大委员会-YOUTH': '00651',
+
 }[x]
 yuanxi = st.selectbox('开课系所', yuanxi_list)
 payload['yuanxi'] = format_yuanxi(yuanxi)
@@ -127,13 +152,16 @@ if st.session_state.show_form:
         st.error("网络连接失败")
     # st.write(payload['startrow'])
     # st.json(response_data)
-    if response_data['status'] == 'ok':
+    if response_data['status'] == 'ok' and response_data['count'] != 0:
         st.success(f"查询成功, 共查询到{response_data['count']}条课程信息，当前展示第{response_data['courselist'][0]['xh']}-{response_data['courselist'][-1]['xh']}条")
     elif response_data['status'] == 'no':
         payload['startrow'] = '0'
         response = requests.post('https://dean.pku.edu.cn/service/web/courseSearch_do.php', data=payload, headers=headers)
         response_data = response.json()
-        st.success(f"查询成功, 共查询到{response_data['count']}条课程信息，当前展示第{response_data['courselist'][0]['xh']}-{response_data['courselist'][-1]['xh']}条")
+        if response_data['status'] == 'ok' and response_data['count'] != 0:
+            st.success(f"查询成功, 共查询到{response_data['count']}条课程信息，当前展示第{response_data['courselist'][0]['xh']}-{response_data['courselist'][-1]['xh']}条")
+        else:
+            st.error("未查询到任何课程信息")
     # print(response.text)
 
     # for course in response['courselist']:
